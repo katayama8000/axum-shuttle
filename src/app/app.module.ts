96843f436136924f40bbb764009806cfcdd5f470
katayama <tattu.0310@gmail.com>
2022-07-24 01:07:32 +0900

    ✨ admin add

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,10 +10,11 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BgCloudComponent } from './bg-cloud/bg-cloud.component';
 import { SigninComponent } from './signin/signin.component';
 import { HomeComponent } from './home/home.component';
+import { AdminComponent } from './admin/admin.component';
 
 
 @NgModule({
-  declarations: [AppComponent, SampleComponent, BgCloudComponent, SigninComponent, HomeComponent],
+  declarations: [AppComponent, SampleComponent, BgCloudComponent, SigninComponent, HomeComponent, AdminComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
